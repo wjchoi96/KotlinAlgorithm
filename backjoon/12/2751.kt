592aa0a2fc25508a24c8c92d4e79bf054cca2fbe
bridgecwj <cwj@bridge.co>
2022-02-26 20:51:36 +0900

    12-2 성공 : 성공인가?  -  O(nlogn)인 정렬 알고리즘으로 풀 수 있습니다. 예를 들면 병합 정렬, 힙 정렬 등이 있지만, 어려운 알고리즘이므로 지금은 언어에 내장된 정렬 함수를 쓰는 것을 추천드립니다.  - 힙, 병합정렬이 기억안나는 상태였으나, 일단 내장 정렬 함수로 해결

---
+++ backjoon/12/2751.kt
@@ -0,0 +1,24 @@
+//sliver5
+//12-2
+
+
+import java.io.*
+fun main(args : Array<String>){
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val size = br.readLine().toInt()
+    val arr : Array<Int> = Array(size){0}
+    for(i in 0 until size){
+        arr[i] = br.readLine().toInt()
+    }
+    arr.sort()
+    for(i in arr){
+        bw.write("$i\n")
+    }
+    
+    bw.flush()
+    bw.close()
+    br.close()
+} 
+
