a69fa261901694925ba478a305560a0a256cab5e
cwj <wjchoi96@naver.com>
2022-03-11 17:23:51 +0900

    15-6 성공

--- backjoon/15/1932.kt
+++ backjoon/15/1932.kt
@@ -46,6 +46,67 @@
 */
 
 
+// import java.io.*
+// import java.util.StringTokenizer
+// lateinit var triArr : Array<Array<Int>>
+// lateinit var triDpArr : Array<Array<Int>>
+// lateinit var bw : BufferedWriter
+// fun main(args : Array<String>){
+//     val br = BufferedReader(InputStreamReader(System.`in`))
+//     bw = BufferedWriter(OutputStreamWriter(System.out))    
+//     var st : StringTokenizer
+
+//     val n = br.readLine().toInt()
+//     triArr = Array(n+1){Array(n){-1}}
+//     triDpArr = Array(n+1){Array(n){-1}}
+
+//     for(i in 0 until n){
+//         st = StringTokenizer(br.readLine())
+//         for(j in 0 until i+1){
+//             triArr[i][j] = st.nextToken().toInt()
+//         }
+//     }
+
+//     var max = Int.MIN_VALUE
+//     for(i in 0 until n){
+//         val res = getTriValue(n-1, i, n)
+//         max = Math.max(res, max)
+//         bw.write("$res\n")
+//     }
+//     bw.write("===> $max\n")
+
+//     br.close()
+//     bw.flush()
+//     bw.close()
+// }
+
+// private fun getTriValue(col : Int, n : Int, size : Int) : Int{
+//     if(col == 0 && n == 0){
+//         bw.write("[$col, $n] => ${triArr[0][0]}\n")
+//         return triArr[0][0]
+//     }else if(col == 1){
+//         bw.write("[$col, $n] => ${triArr[col][n]}\n")
+//         return triArr[0][0] + triArr[col][n]
+//     }
+    
+//     if(triDpArr[col][n] >= 0){
+//         bw.write("[$col, $n] => ${triArr[col][n]}\n")
+//         return triDpArr[col][n]
+//     }
+//     // dp 랑 arr 을 혼동해서 잘못짠거같은데
+//     if(n == 0){
+//         triDpArr[col][n] = getTriValue(col-1, n, size) + triArr[col][n]
+//     }else if(n == size){
+//         triDpArr[col][n] = getTriValue(col-1, n-1, size) + triArr[col][n]
+//     }else{
+//         triDpArr[col][n] = Math.max(getTriValue(col-1, n-1, size) + triArr[col][n], getTriValue(col-1, n, size) + triArr[col][n])
+//     }
+//     bw.write("[$col, $n] => ${triArr[col][n]}\n")
+//     return triDpArr[col][n]
+// }
+
+// 제출용
+
 import java.io.*
 import java.util.StringTokenizer
 lateinit var triArr : Array<Array<Int>>
@@ -56,8 +117,8 @@ fun main(args : Array<String>){
     var st : StringTokenizer
 
     val n = br.readLine().toInt()
-    triArr = Array(n+1){Array(n+1){-1}}
-    triDpArr = Array(n+1){Array(n+1){-1}}
+    triArr = Array(n+1){Array(n){-1}}
+    triDpArr = Array(n+1){Array(n){-1}}
 
     for(i in 0 until n){
         st = StringTokenizer(br.readLine())
@@ -66,10 +127,11 @@ fun main(args : Array<String>){
         }
     }
 
-    for(i in 0 until n + 1){
-        val res = getTriValue(n, i, n)
-        bw.write("$res\n")
+    var max = Int.MIN_VALUE
+    for(i in 0 until n){
+        max = Math.max(getTriValue(n-1, i, n), max)
     }
+    bw.write("$max\n")
 
     br.close()
     bw.flush()
@@ -77,31 +139,21 @@ fun main(args : Array<String>){
 }
 
 private fun getTriValue(col : Int, n : Int, size : Int) : Int{
-    // if (n == 0){
-    //     [n][n] = [n-1][n] + self
-    // }else if(n == n){
-    //     [n][n] = [n-1][n-1] + self
-    // }else{
-    //     [n][n] = Math.max([n-1][n-1] + self, [n-1][n] + self)
-    // }
-
-    print("col : $col, n : $n\n")
     if(col == 0 && n == 0){
         return triArr[0][0]
     }else if(col == 1){
         return triArr[0][0] + triArr[col][n]
     }
     
-    if(triDpArr[col][n] >= 0){
+    if(triDpArr[col][n] != -1){
         return triDpArr[col][n]
     }
-    // dp 랑 arr 을 혼동해서 잘못짠거같은데
     if(n == 0){
-        triDpArr[col][n] = triDpArr[col-1][n] + triArr[col][n]
-    }else if(n == size){
-        triDpArr[col][n] = triDpArr[col-1][n-1] + triArr[col][n]
+        triDpArr[col][n] = getTriValue(col-1, n, size) + triArr[col][n]
+    }else if(n == size-1){
+        triDpArr[col][n] = getTriValue(col-1, n-1, size) + triArr[col][n]
     }else{
-        triDpArr[col][n] = Math.max(triDpArr[col-1][n-1] + triArr[col][n], triDpArr[col-1][n] + triArr[col][n])
+        triDpArr[col][n] = Math.max(getTriValue(col-1, n-1, size) + triArr[col][n], getTriValue(col-1, n, size) + triArr[col][n])
     }
     return triDpArr[col][n]
 }
