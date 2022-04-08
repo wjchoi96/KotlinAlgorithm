1cb254456d17f064251424c272aabe23000fb258
cwj <wjchoi96@naver.com>
2022-04-08 12:03:35 +0900

    프로젝트 정리

---
+++ backjoon/stepquestion/15-greedy/13305.kt
@@ -0,0 +1,16 @@
+// sliver 4
+// 16-5
+
+
+
+import java.io.*
+import java.util.StringTokenizer
+fun main(args : Array<String>){ 
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val br = BufferedReader(InputStreamReader(System.`in`))
+
+   
+    br.close()
+    bw.flush()
+    bw.close()
+}
