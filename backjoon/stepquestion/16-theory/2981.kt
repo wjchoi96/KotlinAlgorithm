8bfc098b49ce5bb837f7c8f23308a98bce3ebfbb
cwj <wjchoi96@naver.com>
2022-04-12 13:52:22 +0900

    16-5 성공 : 접근을 하질 못해서 검색을 통해 접근 및 알고리즘 학습 후 구현, 다시 풀어볼것  - 이해는 했으나, 다시 풀때 접근 해낼 수 있을지 걱정이 된다. 다시 풀어볼것

--- backjoon/stepquestion/16-theory/2981.kt
+++ backjoon/stepquestion/16-theory/2981.kt
@@ -14,30 +14,63 @@
 
     6 32 38
     m 이 하나 이상 무조건 존재
+
+    6%m == 32%m == 38%m
     
     6/m + r
     32/m + r
     38/m + r
 
-    r은 모두 같다
+    r(나머지), m(나눈수) 는 모두 같다
     
+    A1 = a1*m+r
+    A2 = a2*m+r
+    A1-A2 = (a1*m+r) - (a2*m+r)
+    => a1*m - a2*m - r - r  ===>  r - r = 0
+    => m(a1-a2) = A1-A2
 
-    r 을 0으로 만들 수 있다면 m을 쉽게 구할 수 있다
+    (A1-A2)/m = a1-a2
+
+    (6-32)m
+    (32-38)m 은 같은 값 => (6-32) 와 (32-38)의 공약수라는 의미
+
 */
 
 import java.io.*
-import java.util.StringTokenizer 
-
 fun main(args : Array<String>){
     val bw = BufferedWriter(OutputStreamWriter(System.out))
-    val br = BufferedReader(InputStreamReader(System.`in`))    
-    var st : StringTokenizer
-
+    val br = BufferedReader(InputStreamReader(System.`in`))  
     val size = br.readLine().toInt()
-    bw.write("$size\n")
     
+    val arr = Array(size) { 0 }
+    for(i in 0 until size){
+        arr[i] = br.readLine().toInt()
+    }
+    arr.sort()
+
+    var gcdValue = arr[1] - arr[0]
+    for(i in 2 until size){
+        gcdValue = gcd(gcdValue, arr[i]-arr[i-1])
+    }
+    for(i in 2 until gcdValue+1){
+        if(gcdValue%i == 0){
+            bw.write("$i ")
+        }
+    }
+    bw.write("\n")
     
     bw.flush()
     bw.close()
     br.close()
+}
+
+private fun gcd(a : Int, b : Int) : Int {
+    var x = a
+    var y = b
+    while(y!=0){
+        val t = x%y
+        x = y
+        y = t
+    }
+    return x
 }
