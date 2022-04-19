fac6701bad6ba8307aa6e9310964c183cbf546e2
bridgecwj <cwj@bridge.co>
2022-04-19 21:10:05 +0900

    [refactor] 백준 4179 최적화 성공

--- backjoon/algorithmtype/bfs/4179.kt
+++ backjoon/algorithmtype/bfs/4179.kt
@@ -70,6 +70,14 @@
     => 불이 못가는곳을 처리했더니, 지훈이도 못가는곳이 정답처리가 되어버렸다
 
 
+    [refactor] 제출
+    1. 틀렸습니다(7%쯤)
+    1-3 반례로 틀림
+    
+    2. 틀렸습니다(71%쯤)
+    1-2 반례로 틀림
+
+    3. 성공
 */
 
 import java.util.StringTokenizer
@@ -85,6 +93,99 @@ private val fQueue : Queue<Pair<Int, Int>> = LinkedList()
 private val jQueue : Queue<Pair<Int, Int>> = LinkedList()
 fun main(args : Array<String>){
     val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val st = StringTokenizer(br.readLine())
+    val n = st.nextToken().toInt()
+    val m = st.nextToken().toInt()
+
+    board = Array(n){ Array(m){ '.' } }
+    fDisit = Array(n){Array(m){-1}}
+    jDisit = Array(n){Array(m){-1}}
+
+    for(i in 0 until n){
+        val row = br.readLine()
+        for(j in 0 until m){
+            board[i][j] = row[j]
+            when(board[i][j]){
+                'J' -> {
+                    jQueue.offer(Pair(i, j))
+                    jDisit[i][j] = 0
+                }
+                'F' -> {
+                    fQueue.offer(Pair(i, j))
+                    fDisit[i][j] = 0
+                }
+            }
+        }
+    }
+
+    val dx = arrayOf(1, 0, -1, 0)
+    val dy = arrayOf(0, 1, 0, -1)
+    while(!fQueue.isEmpty()){
+        val node = fQueue.poll()
+        for(i in 0 until 4){
+            val nx = node.first + dx[i]
+            val ny = node.second + dy[i]
+            if(nx<0 || nx>= n || ny<0 || ny>=m){
+                continue
+            }
+            // 벽이거나, 방문한 노드라면 건너뛴다
+            if(board[nx][ny] == wall || fDisit[nx][ny] >=0){
+                continue
+            }
+            fQueue.offer(Pair(nx,ny))
+            fDisit[nx][ny] = fDisit[node.first][node.second] + 1
+        }
+    }
+    //fire 의 진행기록이 fDisit에 담겨있다
+
+    while(!jQueue.isEmpty()){
+        val node = jQueue.poll()
+        for(i in 0 until 4){
+            val nx = node.first + dx[i]
+            val ny = node.second + dy[i]
+            
+            if(nx<0 || nx>= n || ny<0 || ny>=m){
+                // 탈출구 도착. 불에 번지기전에 도착한거라면 완료 -> 불에 진행상황을 먼저 체크한다
+                // 불의 진행상황을 먼저 체크했더니, index out of range exception 이 발생할 수 있군
+                // 여긴 size가 밖인곳이니 +1을 안해줘도 되는구나
+                // 여긴 board 를 벗어난 곳이니 여기서 불 disit를 체크할 필요가 없었구나
+                bw.write("${jDisit[node.first][node.second] + 1}") 
+                bw.flush()
+                bw.close()
+                br.close()
+                return
+            }
+
+            //벽이거나, 지훈이보다 불이 먼저 도착한곳이라면 건너뛴다
+            //내가 기존에 방문했던곳인지 체크하는 코드도 누락
+            if(board[nx][ny] == wall || jDisit[nx][ny] >=0){
+                continue
+            }
+            //jDisit[nx][ny] 여기선 초기화값이 들어가있어서 비교할수 없다
+            //fire가 방문하지 못한곳은 초기화값인 -1이 들어가있기때문에 제외해준다
+            if(fDisit[nx][ny] != -1 && (fDisit[nx][ny] <= jDisit[node.first][node.second] + 1)){
+                continue
+            }
+            
+            
+            jQueue.offer(Pair(nx, ny))
+            jDisit[nx][ny] = jDisit[node.first][node.second] + 1
+        }
+    }
+    
+    bw.write("IMPOSSIBLE\n")
+    bw.flush()
+    bw.close()
+    br.close()
+
+}
+
+/*
+    solve 1
+*/
+private fun solve1(){
+     val bw = BufferedWriter(OutputStreamWriter(System.out))
     val br = BufferedReader(InputStreamReader(System.`in`))
     val st = StringTokenizer(br.readLine())
     val n = st.nextToken().toInt()
@@ -153,7 +254,6 @@ fun main(args : Array<String>){
     br.close()
 
 }
-
 private fun bfs(node : Pair<Int, Int>, disit : Array<Array<Int>>, queue : Queue<Pair<Int, Int>>){
     val dx = arrayOf(1, 0, -1, 0)
     val dy = arrayOf(0, 1, 0, -1)
