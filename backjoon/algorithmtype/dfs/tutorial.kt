465f16d6f93e6585d6d25d4ad1b044f8757cbcb3
cwj <wjchoi96@naver.com>
2022-04-22 14:45:17 +0900

    [update] dfs, bfs 튜토리얼 업데이트
     - bfs, dfs를 어떤 유형에 주로 사용하는지 학습
     - bfs, dfs를 구현하는 다른 방법 학습 : dfs를 재귀함수로 구현

--- backjoon/algorithmtype/dfs/tutorial.kt
+++ backjoon/algorithmtype/dfs/tutorial.kt
@@ -24,21 +24,31 @@
     다차원 배열에서의 순회는 bfs가 좀더 유능
     dfs는 그래프, 트리에서 주로 사용된다
 
+    dfs : 재귀호출, 백트래킹, 조건 많은 그래프
+    bfs : 큐, 최단거리, 최소비용, 모든 간선의 비용 1
+
+    dfs 는 스택을 이용한방법, 재귀함수를 이용한 방법으로 구현 가능
+    bfs 는 큐를 이용한 방법, 인접행렬, 인접리스트를 이용한방법(? 알아보자) 구현 가능
+
     =========================================================================
 
     네이버 카페 문제50선 중 dfs 유형 // https://cafe.naver.com/startdevelopercareer
     1. 백준 2667(solve)
     2. 백준 2468(solve)
     3. 백준 10026(solve)
-    
+    4. 백준 1987
 
 
 */
 
 import java.util.Stack
 fun main(args : Array<String>){
-    print("bfs tutorial\n")
+    print("dfs tutorial\n")
+    print("dfs1\n")
     dfs1()
+    print("dfs2\n")
+
+    dfs2(Pair(0,0))
 }
 
 
@@ -47,27 +57,27 @@ fun main(args : Array<String>){
     바킹독님 코드
 */
 
+private val board : Array<Array<Int>> = arrayOf(
+    arrayOf(1,1,1,0,1,0,0,0,0,0),
+    arrayOf(1,0,0,0,1,0,0,0,0,0),
+    arrayOf(1,1,1,0,1,0,0,0,0,0),
+    arrayOf(1,1,0,0,1,0,0,0,0,0),
+    arrayOf(0,1,0,0,0,0,0,0,0,0),
+    arrayOf(0,0,0,0,0,0,0,0,0,0),
+    arrayOf(0,0,0,0,0,0,0,0,0,0),
+    arrayOf(0,1,0,0,0,0,0,0,0,0),
+    arrayOf(0,0,0,0,0,0,0,0,0,0),
+    arrayOf(0,0,0,0,0,0,0,0,0,0)
+)
+private val dx = arrayOf(0, 1, 0, -1)
+private val dy = arrayOf(1, 0, -1, 0)
 private fun dfs1(){
-    val board : Array<Array<Int>> = arrayOf(
-        arrayOf(1,1,1,0,1,0,0,0,0,0),
-        arrayOf(1,0,0,0,1,0,0,0,0,0),
-        arrayOf(1,1,1,0,1,0,0,0,0,0),
-        arrayOf(1,1,0,0,1,0,0,0,0,0),
-        arrayOf(0,1,0,0,0,0,0,0,0,0),
-        arrayOf(0,0,0,0,0,0,0,0,0,0),
-        arrayOf(0,0,0,0,0,0,0,0,0,0),
-        arrayOf(0,1,0,0,0,0,0,0,0,0),
-        arrayOf(0,0,0,0,0,0,0,0,0,0),
-        arrayOf(0,0,0,0,0,0,0,0,0,0)
-    )
     val stack = Stack<Pair<Int, Int>>()
     val visit : Array<Array<Boolean>> = Array(board.size){Array(board[0].size) { false }}
 
     // 상하좌우 4방향을 의미
     // val dx = arrayOf(1, 0, -1, 0)
     // val dy = arrayOf(0, 1, 0, -1)
-    val dx = arrayOf(0, 1, 0, -1)
-    val dy = arrayOf(1, 0, -1, 0)
 
     stack.push(Pair(0, 0))
     visit[0][0] = true
@@ -92,3 +102,42 @@ private fun dfs1(){
         }
     }
 }
+
+// dfs 는 스택을 사용하는 특성상 재귀함수로도 구현이 가능하다
+private val visit2 = Array(board.size){Array(board.size){false}}
+// 보다시피 dest 와 dx, dy 를 반대로 둬봤는데, 탐색루트가 동일하다
+private val dest = arrayOf(Pair(1,0), Pair(0,1), Pair(-1,0), Pair(0,-1))
+// private val dx = arrayOf(0, 1, 0, -1)
+// private val dy = arrayOf(1, 0, -1, 0)
+private fun dfs2(start : Pair<Int, Int>){
+    visit2[start.first][start.second] = true
+    print("node[${start.first}][${start.second}]\n")
+    for(i in 0 until 4){
+        val dx = start.first + dest[i].first
+        val dy = start.second + dest[i].second
+        if(dx<0 || dx>=board.size || dy<0 || dy>=board[0].size){
+            continue
+        }
+        if(visit2[dx][dy] == true || board[dx][dy] != 1){
+            continue
+        }
+        dfs2(Pair(dx, dy)) 
+    }
+}
+/*
+    재귀 방식인 일반 stack 방식과 돌리는 순서가 약간 다른다
+
+    스택은 for문 안에서 나중에 넣어진 node 를 먼저 pop하지만
+    재귀는 재귀호출이 되면 바로 깊이깊이 이어져버리기때문에
+    결과는 같지만, 순서는 다르다(x, y축 우선을 말하는것)
+*/
+
+/*
+    00
+
+    10
+    01
+
+    20
+    01
+*/
