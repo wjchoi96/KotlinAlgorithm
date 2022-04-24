9dc7f74085652fe267000650b81f5cda1f519de2
bridgecwj <cwj@bridge.co>
2022-04-24 17:54:00 +0900

    [add] base code 파일 추가
     - 기초 입출력 코드가 담겨있다

---
+++ baseCode.kt
@@ -0,0 +1,15 @@
+/*
+    기초 입출력 코드
+*/
+import java.io.*
+import java.util.StringTokenizer
+fun main(args : Array<String>){
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    var st = StringTokenizer(br.readLine())
+
+
+    bw.flush()
+    bw.close()
+    br.close()
+}
