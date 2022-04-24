3a1b0e83cab1399aaac1d01fb4eb384117f82d06
bridgecwj <cwj@bridge.co>
2022-04-24 15:40:28 +0900

    [solve] 백준 1629
     - 바킹독님 재귀 단원 강의중 제시된 연습문제

---
+++ backjoon/algorithmtype/recursivefunction/1629.kt
@@ -0,0 +1,81 @@
+//sliver1
+/*
+    바킹독님 재귀함수 단원 강의 중 제시된 연습문제
+
+    A를 B번 곱한 수를 알고싶다
+    구하려는 수가 매우 커질 수 있으니 C로 나눈 나머지를 구하는 프로그램 작성
+
+    1. 반복
+    2. 재귀
+
+    1<= A,B,C <= 2,147,483,647
+
+    범위가 매우 크니 자료형 범위를 생각하며 풀어야한다
+
+    n 을 i 번곱한다
+*/
+
+/*
+    제출
+    1. 메모리 초과
+    - 스택의 메모리를 넘은듯 하다
+
+    2. 성공
+    - 점화식을 사용해서 풀이
+    - 바킹독님 코드 참고
+*/
+
+import java.io.*
+import java.util.*
+fun main(args : Array<String>){
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    var st = StringTokenizer(br.readLine())
+
+    val a = st.nextToken().toInt()
+    val b = st.nextToken().toInt()
+    val c = st.nextToken().toInt()
+
+    val res = nMod2(a, b, c)
+    bw.write("$res\n")
+
+    bw.flush()
+    bw.close()
+    br.close()
+}
+
+/*
+    10, 11, 12
+
+    10, 5, 12
+    10, 2, 12
+    10, 1, 12
+    
+    10%12 = 10
+    10*10%12
+    i = 1 => 100%12(4)
+
+    4*4%12 
+    i = 2 =>  4
+
+    4*4%12 
+    i = 3 => 4*10%12(4)
+    
+*/
+
+private fun nMod2(n : Int, i : Int, r : Int) : Long{
+    if(i == 1){
+        return (n%r).toLong()
+    }
+    var res : Long = nMod2(n, i/2, r)
+    res = res * res % r
+    if(i%2 == 0) return res
+    return res * n % r
+}
+
+private fun nMod(n : Int, i : Int, r : Int) : Int{
+    if(i == 1){
+        return n%r
+    }
+    return nMod(n*n%r, i-1, r)
+}
