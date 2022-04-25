61ab0651899b3a67413f71887096aeaafa0a7e67
cwj <wjchoi96@naver.com>
2022-04-25 11:06:19 +0900

    [solve] 백준 1182
     - 바킹독님 백트래킹 강의 중 제시된 연습문제

--- backjoon/algorithmtype/backtracking/1182.kt
+++ backjoon/algorithmtype/backtracking/1182.kt
@@ -17,8 +17,70 @@
     2. 합이S가 되면 count++
     3. 중복된 수를 뽑지않는다
     4. 동일한 경우의 수가 나오지않게 오름차순으로 수를 뽑는다 => 원본 수열을 정렬해놓은 상태로 수행해야할듯
+    => 같은수를 여러번 뽑을 수 없다
 
     5. 1~N개의 수를 뽑는건데, 종료 조건을 어떻게 처리할지 생각해보자
     - N개의 수열을 뽑는다 생각하고, 뽑는 진행 과정에서 S가 완성이 되는지 체크?
     - 수를 뽑는데, 뽑은 수들의 합이 S가 되거나, S가 완성되지않고 N개의 수열을 다 뽑는다면 종료
 */
+/*
+    제출
+    1. 시간초과(8%)
+    - 오름차순 정렬 + 한번 추가된 idx는 다시 탐색하지 않도록 수정
+
+    2. 시간초과(8%)
+    - 불필요한 가지를 빠르게 제거
+    - 같은 숫자의 조합이 안되도록 오름차순으로만 수열 조합이 가능하게 설정
+
+    3. 틀렸습니다(70~80?)
+    - sum이 s와 동일하면 return을 시켰었는데, return 제거
+    - 현재 수열에 수가 더 추가가 되었을때도 조건을 만족시킬 수 있기 때문
+    반례
+    5 0
+    0 0 0 0 0
+    => 31
+*/
+
+import java.io.*
+fun main(args : Array<String>){
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val br = BufferedReader(InputStreamReader(System.`in`))
+
+    val (n, s) = br.readLine().split(' ').map{it.toInt()}
+    val arr = br.readLine().split(' ').map{it.toInt()}.sorted()
+    val res : Array<Int> = Array(n){0}
+    val visit : Array<Boolean> = Array(n) { false }
+
+    var count = 0
+    fun dfs(idx : Int = 0, depth : Int = 0, sum : Int = 0){
+        if(depth != 0 && sum == s){
+            for(i in 0 until depth){
+                print("${res[i]} ")
+            }
+            print("=> $sum\n")
+            count++
+        }
+        if(depth == n || idx >= n){
+            for(i in 0 until n){
+                print("${res[i]} ")
+            }
+            print("\n")
+            return
+        }
+        for(i in idx until n){
+            if(visit[i] == false){
+                res[depth] = arr[i]
+                visit[i] = true
+                dfs(i + 1, depth + 1, sum + res[depth])
+                visit[i] = false
+            }
+        }
+    }
+
+    dfs()
+    bw.write("$count\n")
+
+    bw.flush()
+    bw.close()
+    br.close()
+}
