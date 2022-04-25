2970d6f1dbbd6d369cf1c11a497fcc2c0d90e51e
bridgecwj <cwj@bridge.co>
2022-04-26 00:56:59 +0900

    [refactor] 백준 14889
     - 네이버 카페 문제 50선 중 완전탐색, 백트래킹 유형
     - 시간초과가 나는 코드의 문제점을 분석, 문제점 찾아내는데 성공

--- backjoon/algorithmtype/backtracking/14889.kt
+++ backjoon/algorithmtype/backtracking/14889.kt
@@ -39,67 +39,49 @@
     4. 성공
     - 꾸역꾸역 아슬하게 성공하네
     - 시간복잡도가 진짜 아슬한 문제인건가
+
+    5. 성공
+    - 숏코딩 구경도중, 별 차이가 없는 코드인데 시간초과가 안떠서 시도
+    - 왜지?
+
+    6. 성공?
+    - dfs2 + checkScore 조합
+
+    7. 원래 dfs를 main 밖에서 돌려본다
+    - 왜성공하지
+
+    8. 성공
+    - 시간초과 떴던 코드를 분석해보니 3번 시간초과 이후 개선점이 문제였었다
+
+    결론 : 오름차순만 잘 시켰으면 되는 문제였다
 */
 
 import java.io.*
+private lateinit var bw : BufferedWriter
+private lateinit var board : Array<Array<Int>>
+private lateinit var visit : Array<Boolean>
+private var n = 0
+private var min : Int = Int.MAX_VALUE
 fun main(args : Array<String>){
-    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    bw = BufferedWriter(OutputStreamWriter(System.out))
     val br = BufferedReader(InputStreamReader(System.`in`))
 
-    val n = br.readLine().toInt()
-    val board : Array<Array<Int>> = Array(n){Array(n){-1}}
-    val visit = Array(n){false}
-
+    n = br.readLine().toInt()
+    board = Array(n){Array(n){-1}}
+    visit = Array(n){false}
     for(i in 0 until n){
         board[i] = br.readLine().split(' ').map{it.toInt()}.toTypedArray()
     }
+    br.close()
 
-    fun finish(){
-        bw.flush()
-        bw.close()
-        br.close()
-    }
-    var min : Int = Int.MAX_VALUE
-    fun checkScore(){
-        var stark = 0
-        var link = 0
-        for(i in 0 until n-1){
-            for(j in i+1 until n){
-                if(visit[i]==true && visit[j]==true){
-                    stark += board[i][j]
-                    stark += board[j][i]
-                }else if(visit[i]==false && visit[j]==false){
-                    link += board[i][j]
-                    link += board[j][i]
-                }
-            }
-        }
-        min = Math.min(min, Math.abs(stark - link))
-        // 절대값 계산이므로 0이 무조건 최소값
-        if(min == 0){
-            bw.write("$min\n")
-            finish()
-            System.exit(0)
-        }
-    }
-    fun dfs(depth : Int = 0, idx : Int = 1){
-        if(depth == n/2){
-            checkScore() // 뽑힌팀은 visit 가 true, 안뽑힌팀은 visit 가 false 가 되어있을것
-            return
-        }
-        for(i in idx until n){
-            if(visit[i] == false){
-                visit[i] = true
-                dfs(depth + 1, i + 1)
-                visit[i] = false
-            }
-        }
-    }
+
     dfs()
     bw.write("$min\n")
 
-    finish()
+    bw.flush()
+    bw.close()
 }
+
 /*
     0,1
     0,2
@@ -112,3 +94,65 @@ fun main(args : Array<String>){
 
     for문 2중첩으로 하는 이유는, 두명을 한번에 구해서 계산하기 위함인가?
 */
+private fun checkScore(){
+    var stark = 0
+    var link = 0
+    for(i in 0 until n-1){
+        for(j in i+1 until n){
+            if(visit[i]==true && visit[j]==true){
+                stark += board[i][j]
+                stark += board[j][i]
+            }else if(visit[i]==false && visit[j]==false){
+                link += board[i][j]
+                link += board[j][i]
+            }
+        }
+    }
+    min = Math.min(min, Math.abs(stark - link))
+    // 절대값 계산이므로 0이 무조건 최소값
+    // if(min == 0){
+    //     bw.write("$min\n")
+    //     bw.flush()
+    //     bw.close()
+    //     System.exit(0)
+    // }
+}
+
+private fun dfs(depth : Int = 0, idx : Int = 0){
+    if(depth == n/2){
+        checkScore() // 뽑힌팀은 visit 가 true, 안뽑힌팀은 visit 가 false 가 되어있을것
+        return
+    }
+    for(i in idx until n){
+        if(visit[i] == false){
+            visit[i] = true
+            dfs(depth + 1, i + 1)
+            visit[i] = false
+        }
+    }
+}
+private fun dfs2(idx : Int = 0, depth : Int = 0){
+    if(depth == n/2){
+        // var stark = 0 
+        // var link = 0
+        // for(i in 0 until n-1){
+        //     for(j in i+1 until n){
+        //         if(visit[i] == true && visit[j] == true){
+        //             stark += board[i][j] + board[j][i]
+        //         }else if(visit[i] == false && visit[j] == false){
+        //             link += board[i][j] + board[j][i]
+        //         }
+        //     }
+        // }
+        // min = Math.min(min, Math.abs(stark - link))
+        checkScore()
+        return
+    }
+    for(i in idx until n){
+        if(visit[i] == false){
+            visit[i] = true
+            dfs2(i + 1, depth + 1)
+            visit[i] = false
+        }
+    }
+}
