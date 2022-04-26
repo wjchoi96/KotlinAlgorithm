23a3f9ef06dd623c5c63cb6c12aa92a197791b5c
cwj <wjchoi96@naver.com>
2022-04-26 15:20:42 +0900

    [solve] 백준 2661
     - 네이버 카페 문제 50선 중 백트래킹, 완전탐색 유형
     - 백트래킹 자체의 난이도는 쉬웠지만, 좋은수열 나쁜수열 판별하는 코드 구현이 까다로웠다

--- backjoon/algorithmtype/backtracking/2661.kt
+++ backjoon/algorithmtype/backtracking/2661.kt
@@ -38,6 +38,94 @@
     1. 같은 수가 연속으로 올때
     2. 현재idx 부터 2, 3, 4 단위로 수열을 쪼갰을때 동일한게 있다면
 */
+/*
+    제출
+    1. 런타임에러(9%) : NumberFormat
+    - 80자리의 숫자를 Int형으로 변환 시도 -> 펑
+    - 비교할 필요없이 무조건 첫번째 나오는 착한수열이 최소값이다
+    => 1부터 시작하는 dfs이기 때문
+
+    2. 맞았습니다
+
+
+*/
+
+
+import java.io.*
+private lateinit var res : Array<Int>
+private var n = 0
+private lateinit var bw : BufferedWriter
+fun main(args : Array<String>){
+    bw = BufferedWriter(OutputStreamWriter(System.out))
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    n = br.readLine().toInt()
+    br.close()
+    res = Array(n){0}
+
+    dfs()
+
+    bw.flush()
+    bw.close()
+}
+
+/*
+    [1,2,3] 을 뽑는다
+    N개까지
+*/
+private fun dfs(depth : Int = 0){
+    if(depth == n){
+        print("good : ")
+        for(i in 0 until n){
+            print("${res[i]} ")
+        }
+        print("\n")
+        var str = ""
+        for(i in 0 until n){
+            str += res[i].toString()
+        }
+        bw.write("$str\n")
+        bw.flush()
+        bw.close()
+        System.exit(0)
+        return
+    }
+    for(i in 1 until 4){
+        res[depth] = i
+        if(checkBadArr(depth)){
+            dfs(depth + 1)
+        }
+    }
+}
+
+
+// appendIdx : 방금 추가된 item의 idx, 해당 idx로 접근하면 item이 존재한다
+private fun checkBadArr(appendIdx : Int) : Boolean { 
+    val size = appendIdx + 1
+    var str = ""
+    for(i in 0 until size){
+        str += res[i].toString()
+    }
+    print("str[$appendIdx] : $str\n")
+    for(childSize in 1 until size/2+1) {
+        // 1, 2, 3, 4 ... size/2 까지 순회
+        // 마지막 childSize개와 그 앞 childSize가 동일한지 체크
+        val firstIdx = size - childSize
+        val secondIdx = size - (childSize*2)
+        // print("f : $firstIdx, s : $secondIdx, c : $childSize s : ${size/1}\n")
+        if(firstIdx < 0 || secondIdx < 0) {
+            continue
+        }
+        val first = str.substring(firstIdx until size)
+        val second = str.substring(secondIdx until firstIdx)
+        print("$first : $second\n")
+        if(first == second){
+            print("bad : $str\n")
+            return false
+        }
+    }
+
+    return true
+}
 
 /*
     idx = 1 => size = 2
@@ -49,20 +137,22 @@
     
     근데 그럼 80번째 item을 놓을때는
     1, 2, 3, ... 40 까지 부분수열을 체크해줘야하나?
+
+    !!!!!!!!!!!!
+    //https://bellog.tistory.com/43
+    마지막에 추가된 수를 기준으로
+    마지막 1개와 그 앞 1개가 동일한지
+    마지막 2개와 그 앞 2개가 동일한지
+    마지막 3개와 그 앞 3개가 동일한지
+    ...
+    이런식으로 검색
+    size + size =< arrSize 일떄만 가능
+
+    appendIdx : 8
+    size : 9
+
+    1,2,3,4 => childIdx
+
+    res[8], res[7] => first 8, second 7
+    res[7]res[8], res[5]res[6] => first 7, second 5
 */
-private fun checkBadArr(idx : Int){ // 값이 입력된 부분까지의 idx
-    val size = idx + 1
-}
-
-import java.io.*
-private lateinit var arr : Array<Int>
-private var n = 0
-fun main(args : Array<String>){
-    val bw = BufferedWriter(OutputStreamWriter(System.out))
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    n = br.readLine().toInt()
-
-    bw.flush()
-    bw.close()
-    br.close()
-}
