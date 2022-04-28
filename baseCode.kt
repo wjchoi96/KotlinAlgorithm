ce4374700ed8b4337e6aa24c9cdbbc57f845de74
bridgecwj <cwj@bridge.co>
2022-04-28 23:56:23 +0900

    [refactor] buffered writer, reader 의 초기화 코드 변경
     - java.io.BufferedWriter, Reader 을 import 안해줘도 된다

--- baseCode.kt
+++ baseCode.kt
@@ -2,13 +2,11 @@
 /*
     기초 입출력 코드
 */
-import java.io.*
 import java.util.StringTokenizer
 fun main(args : Array<String>){
-    val bw = BufferedWriter(OutputStreamWriter(System.out))
-    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = System.out.bufferedWriter()
+    val br = System.`in`.bufferedReader()
     var st = StringTokenizer(br.readLine())
-
 
     bw.flush()
     bw.close()
