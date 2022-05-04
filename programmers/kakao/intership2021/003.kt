71b5c87418ac8b3407e961aaf820918f62e162c9
cwj <wjchoi96@naver.com>
2022-05-04 14:21:21 +0900

    [docs] 카카오 2021 인턴 기출문제 3번 풀면서 공부한것들 주석 정리

--- programmers/kakao/intership2021/003.kt
+++ programmers/kakao/intership2021/003.kt
@@ -34,27 +34,41 @@
 */
 
 /*
+
+*/
+
+/*
     수정 이력
+
+    0. arrayList 를 사용하는 방식으로 시도
+    - tc 2~3개 제외하고 다틀림
+    - 5번과정으로 넘어갔음
 
     1. try1 방식으로는 문제를 못찾을것같아서 카카오 공식 문제 해설을 확인
     현재 방식으로 변경
     - tc 반타작
 
-    2. remove 이후 삭제되지 않은 행을 선 택해야한다
+    2. remove 이후 삭제되지 않은 행을 선택해야한다
     - tc 2개 런타임에러
     - 효율성 개박살
 
     3. 2번에서 OX 문자열 설정하는것을 String+ 연산이 아닌 StringBuilder append로 변경
     - tc 2개 런타임에러
     - 효율성 절반 통과
+    - up, down때 배열을 순회하면서 삭제되지않은 행을 찾아서 이동하기떄문에 최악의 경우O(n)의 시간복잡도
+    - 이거때문에 2개 런타임에러가 뜨는거같다
 
-    4. 원랴방식에서 배열을 제거하고 size, pointer 로만 접근하는 방식 수행
+    4. 원래방식(1번)에서 배열을 제거하고 size, pointer 로만 접근하는 방식 수행
     - StringBuilder의 insert 연산의 시간복잡도를 알아내보자
-    - insertAt은 O(1), insert는O(n)이라고 기억
+    - insert(at)은 O(1), insert는O(n)이라고 기억
+    - insert(at)이 아닌 setCharAt메소드가 O(1)이였다 => 해당 위치의 값을 변경하는 메소드
     //https://loosie.tistory.com/339
     =>>> 정확성 효율성 모두 통과!
 
-    4. arrayList 사용하는 방식도 가능성은 있다. restore 할때 p를 조정 안해주는게 문제였던거 같다
+    5. arrayList 사용하는 방식도 가능성은 있다. restore 할때 p를 조정 안해주는게 문제였던거 같다
+    => 정확성 통과, 효율성은 2개 제외하고 모두 시간초과
+    - 이는 arrayList의 add, add(at)연산이 O(n)이여서 그렇다
+    - arrayList를 사용해서는 효율성을 통과할 수 없다고 판단된다
 
     5. linkedList로 구현해보자
 */
@@ -68,14 +82,14 @@ fun main(args : Array<String>){
     val n = 8
     val k = 2
 
-    val res = Kakao003Try1().solution(n, k, cmd)
+    val res = Kakao003Solve().solution(n, k, cmd)
     print("$res\n")
 }
 // 처음부터 다시 해보자
 // LinkedList를 사용안하더라도 정확성 테스트는 통과할수있다는데
 // StringBuilder 를 사용하니까 효율성도 어느정도 통과하는게 생기네
 // 실패 (런타임 에러) 대체 뭔데
-private class Kakao003 {
+private class Kakao003Solve {
     private var removeStack = Stack<Int>()
     private var size = 0
     private var p = 0
@@ -142,15 +156,15 @@ private class Kakao003Try2 {
                 "Z" -> restore()
             }
         }
-        var res = ""
+        var res = StringBuilder()
         for(i in 0 until n){
             if(list[i] == 0){
-                res+="X"
+                res.append("X")
             }else{
-                res+="O"
+                res.append("O")
             }
         }
-        return res
+        return res.toString()
     }
     private fun up(count : Int){
         var upCount = count
@@ -187,6 +201,16 @@ private class Kakao003Try2 {
 
 // try 1 => 3개 빼고 다 fail
 // restore시 p조정을 안해준 문제 발견
+/*
+    tc 통과
+    효율성은 2개 빼고 시간초과
+
+    ArrayList는 get은 예상대로 O(1)이지만
+    add(addAt 포함)은 O(N)이였다
+    add과정은 처음 기본size 10 이후부터는 새 배열을 만들고 복사 후 옮기게 되는 과정
+    add(at)은 특정위치 다음에 존재하는 데이터를 복사 후 한칸 뒤에 붙이는 과정
+    때문에 arrayList로는 효율성을 통과 할 수가 없다
+*/
 private class Kakao003Try1 {
     private var removeStack = Stack<Int>()
     private val list : ArrayList<Int> = ArrayList<Int>()
@@ -205,28 +229,30 @@ private class Kakao003Try1 {
                 "Z" -> restore()
             }
         }
-        var res = ""
+        var res = StringBuilder()
         for(i in 0 until n){
-            if(removeStack.contains(i)){
-                res += "X"
-            }else{
-                res += "O"
-            }
+            res.append("O")
         }
-        
-        return res
+        /*
+            insert는 성공하고 charAt은 실패하는 경우가 있는 이유
+            removeStack에 저장된 idx들은 삭제될때 당시의 list의 size에 대응되는 idx이다
+            때문에 처음부터 원래 size를 가져다놓고, setCharAt을 하면 틀린다
+            stack pop횟수에 따른(몇번째 pop인지) 추가 idx 계산을 해주면 될듯?
+            => 아니다 이건 여기서는 못쓴다
+            => linkedList나 배열을 사용해서 idx가 remove, restore을 해도 원래 자리대로 유지되는 상태에서만 가능
+        */
+        var count = 0
+        while(!removeStack.isEmpty()){
+            res.setCharAt(removeStack.pop()+count++, 'X')
+        }
+        return res.toString()
     }
     private fun select(to : Int){
         p+=to // 범위를 벗어나게 하는 값도 나올까?
-        if(p<0){
-            p = 0
-        }else if(p>=list.size){
-            p = list.size-1
-        }
-        print("select to[$to] => $p\n")
+        // print("select to[$to] => $p\n")
     }
     private fun remove(){
-        print("remove : $p\n")
+        // print("remove : $p\n")
         // 가장 아래인경우 한칸 위로 옮긴다
         // 그 외에는 remove를 수행하면 자연스레 아래칸이 위로 올라오므로 그대로 두면 된다
         removeStack.push(p)
@@ -234,14 +260,15 @@ private class Kakao003Try1 {
         if(p == list.size){
             p-=1
         }
-        printSheet()
     }
     // remove 되었던게 마지막 행인경우, addAt 을 호출해봐야 마지막 행으로 들어가게 될것
     private fun restore(){
         val restore = removeStack.pop()
-        print("restore : $restore\n")
+        // print("restore : $restore\n")
         list.add(restore, restore) // 삭제되었던 idx에 다시 집어넣어준다
-        printSheet()
+        if(restore <= p){ // 현재 선택항목보다 위에 삽입되었다면 p 조정
+            p++
+        }
     }
     private fun printSheet(){
         print("print sheet\n")
--- programmers/kakao/intership2021/tutorial.kt
+++ programmers/kakao/intership2021/tutorial.kt
@@ -7,6 +7,53 @@
     3. [solve] 표 편집(level 2) : 003.kt => 첫날 1시간반, 둘쨰날 15분
     - 4번의 걸친 수정(방식을 변경해보기도함) 끝에 성공
     - 검색의 도움을 받음
+    - 바킹독님 블로그에도 풀이가 있다. 참고해봐도 좋을듯
 
     
+*/
+
+// 진행하며 알게된것들
+/*
+    https://yeon-kr.tistory.com/152
+    ArrayList 연산의 시간복잡도
+
+    ArrayList는 get은 예상대로 O(1)이지만
+    add(addAt 포함)은 O(N)이였다
+    add과정은 처음 기본size 10 이후부터는 새 배열을 만들고 복사 후 옮기게 되는 과정
+    add(at)은 특정위치 다음에 존재하는 데이터를 복사 후 한칸 뒤에 붙이는 과정
+    때문에 arrayList로는 효율성을 통과 할 수가 
+*/
+
+/*
+    https://dundung.tistory.com/95
+    https://barbera.tistory.com/45
+
+    StringBilder, String
+
+    String의 +, += 연산의 경우 O(N+K)의 시간복잡도
+    기존문자열 길이 + 더하려는 문자열 크기
+    
+    StringBilder, StringBuffer은 동기화 지원 여부를 제외하고 동일한 기능을 가진다
+    builder는 지원을 하지않아 멀티스레드에서 안전하지않다
+    buffer은 지원을해서 멀티스레드에서 안전하지만, 동기화과정에서 오버헤드가 발생하여 느릴 수 있다
+
+    StringBilder : 동적배열
+    append : O(1) 
+    delete : O(n)
+    insert : O(n)
+
+    동적배열에서의 추가작업은 배열의 크기를 늘리고, 전 배열을 System.arraycopy메소드를 통해 복붙하는 방식
+    때문에 시간복잡도가 O(n)인데
+    StringBuilder는 배열의 크기를 늘리때 전에 크기의 2배씩 확장하기때문에
+    크기를 늘리는 O(n)작업이 자주 발생하지않는다
+    때문에 상환복잡도를 계산하면 amoritized(분할상환 분석) O(1)음을 할 수 있다
+    떄문에 append의 평균 복잡도는 O(1)
+
+    https://loosie.tistory.com/339
+    setCharAt : O(1)
+    => insert가 아닌 해당 위치의 값을 변경하는 메소드
+    구글링해서 알아낸건데 StringBuffer나 StringBuilder에 setCharAt(int idx, char c)이라는 메소드가 있다. 
+    이는 시간복잡도 O(1)로 문자열 idx의 값을 c로 변경해주는 함수로 제외된 행의 번호 데이터만 있으면 쉽게 답을 출력할 수 있다
+
+
 */
