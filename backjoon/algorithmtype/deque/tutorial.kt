261daaf91777a3c81b055aaba6b5b76f2c3854b2
cwj <wjchoi96@naver.com>
2022-05-16 21:44:07 +0900

    [docs] 덱 콜렉션 함수 종류 추가

--- backjoon/algorithmtype/deque/tutorial.kt
+++ backjoon/algorithmtype/deque/tutorial.kt
@@ -21,6 +21,38 @@
     - head - 가장 앞의 원소를 가리킴/ tail - 가장 뒤의 원소+1을 가리킴
     - 원형구현이 가능하지만, 선형으로 구현
 
+    덱 콜렉션
+    LinkedList 로 구현하는게 좋을것(처음과 끝에 추가가O(1))
+
+    데이터 추가
+    offerFirst, offerLast (add 도 가능)
+    offer과 add의 차이점
+    => 용량초과시 add는 Exception을, offer은 false를 리턴
+    push: offerFirst 과 같다
+
+    데이터 삭제
+    pollFrist, pollLast (remove 도 가능)
+    poll과 remove의 차이점
+    => 비어있을때 remove는 예외, poll은 null 리턴
+    pop: removeFirst와 같다
+
+    데이터 검색 후 삭제(indexFirst and remove 와 같다)
+    removeFirstOccurrence(object): 앞에서 탐색 시작, 첫번째 값 제거
+    removeLastOccurrence(object): 뒤에서 탐색 시작, 첫번째 값 제거
+    remove(object): removeFirstOccurence와 같다
+
+    => 대체로 Last, First가 안들어가면 First가 붙는 효과가 나는것 같다
+
+    데이터 확인
+    first, peekFirst, peek
+    - 값이 없을때 peek: null, first: 예외
+    last, peekLast
+    - 값이 없을때 peek: null, last: 예외
+
+    contain: 포함되어있는지 확인
+    size: 개수
+
+
     연습문제
     - 백준 10866(solve)
 
