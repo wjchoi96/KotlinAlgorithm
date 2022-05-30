5784ba0ec0ed8f0c8482a8c7f7fa061dd707f8d2
cwj <wjchoi96@naver.com>
2022-05-30 10:33:51 +0900

    [refactor] code style 변경

--- src/backjoon/stepquestion/11-sort/11650.kt
+++ src/backjoon/stepquestion/11-sort/11650.kt
@@ -4,97 +4,103 @@
 import java.io.*
 import java.util.StringTokenizer
 fun main(args : Array<String>){
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    val bw = BufferedWriter(OutputStreamWriter(System.out))
-    var st : StringTokenizer
-
-    val size = br.readLine().toInt()
-    val arr = Array<Point>(size){Point(0,0)}
-    for(i in 0 until size){
-        st = StringTokenizer(br.readLine())
-        arr[i] = Point(st.nextToken().toInt(), st.nextToken().toInt())
-    }
-
-    quickSort(arr, 0, size - 1)
-
-    for(i in arr){
-        bw.write("${i.x} ${i.y}\n")
-    }
-    
-    bw.flush()
-    bw.close()
-    br.close()
+    Solution11650().solve()
 }
 
-private fun quickSort(a : Array<Point>, start : Int, end : Int){
-    var pl = start
-    var pr = end
-    var x = a[(start + end)/2]
+class Solution11650 {
+    fun solve(){
+        val br = BufferedReader(InputStreamReader(System.`in`))
+        val bw = BufferedWriter(OutputStreamWriter(System.out))
+        var st : StringTokenizer
 
-    print("a[$start]~a[$end] : {")
-    for(i in a){
-        print("$i ")
-    }
-    print("}\n")
-
-    do{
-        // pl 포인터의 값이 피벗보다 작다면
-        while(a[pl].compareX(x) < 0)pl++ 
-        // pr 포인터의 값이 피벗보다 크다면
-        while(a[pr].compareX(x) > 0)pr-- 
-        if(pl<=pr){
-            swap(a, pl++, pr--)
+        val size = br.readLine().toInt()
+        val arr = Array<Point>(size){Point(0,0)}
+        for(i in 0 until size){
+            st = StringTokenizer(br.readLine())
+            arr[i] = Point(st.nextToken().toInt(), st.nextToken().toInt())
         }
-    }while(pl<=pr);
 
-    if(start<pr) quickSort(a, start, pr)
-    if(pl<end) quickSort(a, pl, end)
-}
+        quickSort(arr, 0, size - 1)
 
-// 시간초과 -> 시간복잡도가 더 낮은 sort 를 찾아야한다
-private fun bubbleSort(arr : Array<Point>, size : Int){
-    for(i in 0 until size){
-        for(j in size-1 downTo i+1){
-            if(arr[j-1].compareX(arr[j]) == 1){
-                swap(arr, j-1, j)
+        for(i in arr){
+            bw.write("${i.x} ${i.y}\n")
+        }
+
+        bw.flush()
+        bw.close()
+        br.close()
+    }
+
+    private fun quickSort(a : Array<Point>, start : Int, end : Int){
+        var pl = start
+        var pr = end
+        val x = a[(start + end)/2]
+
+        print("a[$start]~a[$end] : {")
+        for(i in a){
+            print("$i ")
+        }
+        print("}\n")
+
+        do{
+            // pl 포인터의 값이 피벗보다 작다면
+            while(a[pl].compareX(x) < 0)pl++
+            // pr 포인터의 값이 피벗보다 크다면
+            while(a[pr].compareX(x) > 0)pr--
+            if(pl<=pr){
+                swap(a, pl++, pr--)
+            }
+        }while(pl<=pr);
+
+        if(start<pr) quickSort(a, start, pr)
+        if(pl<end) quickSort(a, pl, end)
+    }
+
+    // 시간초과 -> 시간복잡도가 더 낮은 sort 를 찾아야한다
+    private fun bubbleSort(arr : Array<Point>, size : Int){
+        for(i in 0 until size){
+            for(j in size-1 downTo i+1){
+                if(arr[j-1].compareX(arr[j]) == 1){
+                    swap(arr, j-1, j)
+                }
             }
         }
     }
-}
 
-private fun swap(arr : Array<Point>, idx1 : Int, idx2 : Int){
-    val t = arr[idx1]
-    arr[idx1] = arr[idx2]
-    arr[idx2] = t
-}
+    private fun swap(arr : Array<Point>, idx1 : Int, idx2 : Int){
+        val t = arr[idx1]
+        arr[idx1] = arr[idx2]
+        arr[idx2] = t
+    }
 
-private class Point internal constructor(
-    val x : Int,
-    val y : Int
-){
-    fun compareX(other : Point) : Int {
-        return if(this.x > other.x){
-            1
-        }else if(this.x == other.x){
-            if(this.y > other.y){
+    private class Point internal constructor(
+        val x : Int,
+        val y : Int
+    ){
+        fun compareX(other : Point) : Int {
+            return if(this.x > other.x){
                 1
-            }else if(this.y==other.y){
-                0
+            }else if(this.x == other.x){
+                if(this.y > other.y){
+                    1
+                }else if(this.y==other.y){
+                    0
+                }else{
+                    -1
+                }
             }else{
                 -1
             }
-        }else{
-            -1
+        }
+
+        override fun toString() : String {
+            return "($x $y)"
         }
     }
-
-    override fun toString() : String {
-        return "($x $y)"
-    }
-}
 
 /*
     좌표 n개가 주어진다
     1. x좌표 순
     2. x좌표가 같다면 y좌표 순
 */
+}
