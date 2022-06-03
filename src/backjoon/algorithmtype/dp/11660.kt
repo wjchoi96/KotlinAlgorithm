ea5105cd996260a27fb8a1b23f7e12314766e801
wjchoi96 <wjchoi96@naver.com>
2022-06-04 00:13:18 +0900

    [refactor] 백준 11660 코드 개선
     - 네이버 카페 문제 50선 중 구간합 유형 문제
     - 1-indexed 방식으로 메모이제이션을 진행하도록 변경하여 0일때를 고려하는 코드를 제거
     - bottom-top 방식으로 메모이제이션을 진행하도록 변경하여 data를 입력받으며 메모이제이션 진행

--- src/backjoon/algorithmtype/dp/11660.kt
+++ src/backjoon/algorithmtype/dp/11660.kt
@@ -71,11 +71,119 @@
     2. 성공
     - https://subbak2.tistory.com/65 를 참고하여 풀었다
     - dp를 1-indexed로 변경하면 코드 개선이 될것 같다
+
+    3. 성공
+    - dp를 1-indexed 로 변경 
+    - 0일때를 고려하는 코드가 싹 사라져서 훨씬 개선되었다
+    - bottom top 방식으로 data input 받으며 진행되도록 개선해보자
+
+    4. 성공
+    - data 를 input 받으며 bottom-top 방식으로 메모이제이션 진행
+ 
 */
 
+import java.util.StringTokenizer // use at Solution11660C
 fun main(args: Array<String>){
-    Solution11660().solve()
+    Solution11660C().solve()
 }
+
+// data 를 input 받으며 bottom-top 방식으로 메모이제이션 진행 -> 4번 제출
+class Solution11660C {
+    private lateinit var dp: Array<Array<Int>>
+    fun solve(){
+        val br = System.`in`.bufferedReader()
+        val bw = System.out.bufferedWriter()
+
+        // input data
+        val (n,m) = br.readLine().split(' ').map{it.toInt()}
+        dp = Array(n+1){Array(n+1){-1}}
+        for(i in 0..n){
+            dp[i][0] = 0
+            dp[0][i] = 0
+        }
+
+        var st: StringTokenizer
+        for(x in 1..n){
+            st = StringTokenizer(br.readLine())
+            for(y in 1..n){
+                dp[x][y] = dp[x-1][y] + dp[x][y-1] - dp[x-1][y-1] + st.nextToken().toInt()
+            }
+        }
+
+        repeat(m){
+            val (x1, y1, x2, y2) = br.readLine().split(' ').map{it.toInt()}
+            bw.write("${getBoardSum(x1, y1, x2, y2)}\n")
+        }
+        
+        bw.flush()
+        br.close()
+        bw.close()
+    }
+    private fun getBoardSum(xa: Int, ya: Int, xb: Int, yb: Int): Int{
+        val x1 = Math.max(xa, xb)
+        val x2 = Math.min(xa, xb)
+        val y1 = Math.max(ya, yb)
+        val y2 = Math.min(ya, yb)
+        return dp[x1][y1] - dp[x1][y2-1] - dp[x2-1][y1] + dp[x2-1][y2-1]
+    }
+}
+
+// dp를 1-indexed로 변경하여 개선 된 코드 -> 3번 제출
+class Solution11660B {
+    private lateinit var board: Array<Array<Int>>
+    private lateinit var dp: Array<Array<Int>>
+    fun solve(){
+        val br = System.`in`.bufferedReader()
+        val bw = System.out.bufferedWriter()
+
+        // input data
+        val (n,m) = br.readLine().split(' ').map{it.toInt()}
+        board = Array(n){Array(n){0}}
+        dp = Array(n+1){Array(n+1){-1}}
+        for(i in 0..n){
+            dp[i][0] = 0
+            dp[0][i] = 0
+        }
+        repeat(n){
+            board[it] = br.readLine().split(' ').map{it.toInt()}.toTypedArray()
+        }
+        getBoardDp(n, n)
+        repeat(m){
+            val (x1, y1, x2, y2) = br.readLine().split(' ').map{it.toInt()}
+            bw.write("${getBoardSum(x1, y1, x2, y2)}\n")
+        }
+        
+        bw.flush()
+        br.close()
+        bw.close()
+    }
+
+    //top-bottom
+    private fun getBoardDp(x: Int, y: Int): Int{
+        if(dp[x][y] != -1) return dp[x][y]
+        dp[x][y] = getBoardDp(x-1, y) + getBoardDp(x, y-1) - getBoardDp(x-1, y-1) + board[x-1][y-1]
+        return dp[x][y]
+    }
+    private fun getBoardSum(xa: Int, ya: Int, xb: Int, yb: Int): Int{
+        val x1 = Math.max(xa, xb)
+        val x2 = Math.min(xa, xb)
+        val y1 = Math.max(ya, yb)
+        val y2 = Math.min(ya, yb)
+        return dp[x1][y1] - dp[x1][y2-1] - dp[x2-1][y1] + dp[x2-1][y2-1]
+    }
+    private fun Array<Array<Int>>.print() {
+        for(x in 0 until this.size){
+            for(y in 0 until this.size){
+                print("${this[x][y]} ")
+            }
+            print("\n")
+        }
+    }
+
+}
+
+// 0-indexed 방식으로 해결
+// 개선 이전 코드 -> 2번 제출
 class Solution11660 {
     private lateinit var board: Array<Array<Int>>
     private lateinit var dp: Array<Array<Int>>
