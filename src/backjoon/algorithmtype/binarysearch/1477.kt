23afd3c611c8d28e9138551af07a136cabad70d4
cwj <wjchoi96@naver.com>
2022-06-14 17:37:47 +0900

    [add] 백준 1477
     - 바킹독님 알고리즘 강의 중 이분탐색 parametic search 유형 문제

---
+++ src/backjoon/algorithmtype/binarysearch/1477.kt
@@ -0,0 +1,25 @@
+/*
+    바킹독님 알고리즘 강의 중 그리디 단원 잘못된 그리디 유형 -> parametric search 유형의 문제라고 소개 된 문제
+    바킹독님 알고리즘 강의 중 이분탐색 단원 parametric search 부분 학습 중 풀이 
+    gold 4
+    https://www.acmicpc.net/problem/1477
+
+    휴게소 세우기
+
+    
+
+*/
+
+fun main(args: Array<String>){
+    Solution1477().solve()
+}
+class Solution1477 {
+    fun solve(){
+        val bw = System.out.bufferedWriter()
+        val br = System.`in`.bufferedReader()
+    
+        bw.flush()
+        bw.close()
+        br.close()
+    }
+}
