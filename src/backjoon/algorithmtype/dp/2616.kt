2e98a24051413b025c07ac71ca969b699cf8b905
cwj <wjchoi96@naver.com>
2022-06-20 17:29:04 +0900

    [add] 백준 2616
     - 네이버 카페 문제 50선 중 구간합 유형 연습문제

---
+++ src/backjoon/algorithmtype/dp/2616.kt
@@ -0,0 +1,24 @@
+/*
+    네이버 카페 문제 50선 중 구간합 유형 문제
+    gold 4
+
+    소형기관차
+    https://www.acmicpc.net/problem/2616
+
+    
+
+*/
+
+fun main(args: Array<String>){
+    Solution2616().solve()
+}
+class Solution2616 {
+    fun solve(){
+        val bw = System.out.bufferedWriter()
+        val br = System.`in`.bufferedReader()
+    
+        bw.flush()
+        bw.close()
+        br.close()
+    }
+}
